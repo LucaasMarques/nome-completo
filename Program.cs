b261d331f40183ed362f438710ad289fe7f0274d
lucas <lucsalmeida004@gmail.com>
2025-09-26 19:41:15 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("digite seu primeiro nome");
+string nome = Console.ReadLine();
+Console.WriteLine("digite seu sobrenome");
+string sobrenome =Console.ReadLine();
+Console.WriteLine( $"nome completo {nome} {sobrenome}" );
+Console.WriteLine( $"nome de catalogo: {sobrenome.ToUpper()} {nome}");
+
+
+
+
